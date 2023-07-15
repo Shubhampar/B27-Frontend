c28f14f962373f7f52e291f0f2ee6f6725470d13
Shubhampar <119346050+Shubhampar@users.noreply.github.com>
2023-07-15 23:16:27 +0530

    push-3

--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -4,26 +4,20 @@ import "./Notes.css"
 export const Notes = () => {
   const [notes, setNotes] = useState([]);
   useEffect(() => {
-    fetch("https://lovely-red-magpie.cyclic.app/notes", {
+    fetch("http://localhost:4500/notes", {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
         authorization: `Bearer ${localStorage.getItem("token")}`
       }
     }).then(res => res.json())
-    .then(data => {
-      if (Array.isArray(data)) {
-        setNotes(data);
-      } else {
-        setNotes([]); // If data is not an array, set notes as an empty array
-      }
-    })
+      .then(data => setNotes(data))
       .catch(err => console.log(err))
   }, [])
   return (
     <div>
       <h2>To see the notes</h2>
-      {Array.isArray(notes) && notes.map(note => (
+      {notes.map(note => (
         <div key={note.id} className="note-box">
           <h3>{note.title}</h3>
           <p>{note.body}</p>
